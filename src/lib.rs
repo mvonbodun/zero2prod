7f4e0b1ebbb2eefece80e421179c52461214b344
Michael von Bodungen <michael@vonbodungen.com>
2020-09-02 09:02:42 -0500

    fixed format errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -9,5 +9,5 @@ pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
         .run();
-        Ok(server)
+    Ok(server)
 }
