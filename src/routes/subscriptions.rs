590ac0ce88b50aa4941a962caad8d1c7b1d8cf3d
Michael von Bodungen <michael@vonbodungen.com>
2024-10-14 20:24:05 -0500

    after sqlx prepare step

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,51 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "adding a new subscriber",
+    skip(form, pg_pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, pg_pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&form, &pg_pool).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pg_pool)
+)]
+pub async fn insert_subscriber(form: &FormData, pg_pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(pg_pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute queryj: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
